4849a32354218934b31759d05cdcd2144341f350
ahitsaadr <you@example.com>
2023-10-20 11:00:07 +0700

    Restfull API

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,7 +8,12 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('insertproduct', 'ProductController::insertPage');
 $routes->get('readproduct', 'ProductController::readProducts');
-$routes->get('edit-product/(:any)', 'ProductController::getProduct/$1');
-$routes->post('edit-product/(:any)', 'ProductController::updateProduct/$1');
-$routes->get('delete-product/(:any)', 'ProductController::deleteProduct/$1');
 $routes->post('insertproduct', 'ProductController::insertProductORM');
+$routes->get('edit-product/(:any)', 'ProductController::getProduct/$1');
+$routes->get('edit-product/(:any)', 'ProductController::updateProduct/$1');
+$routes->post('product-update/(:any)', 'ProductController::updateProduct/$1');
+$routes->get('delete-product/(:any)', 'ProductController::deleteProduct/$1');
+$routes->group('api', function($routes){
+    $routes->get('products','ProductController::readProductApi');
+    $routes->get('product/(:any)','ProductController::getProductApi/$1');
+});
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,18 +3,18 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProductModel;
+use CodeIgniter\API\ResponseTrait;
 
-class ProductController extends BaseController
-{
+class ProductController extends BaseController{
+use ResponseTrait;
+
     private $product;
 
     public function __construct()
     {
         $this->product = new ProductModel();
     }
-
-    public function readProducts()
-    {
+    public function readProducts(){
         $products =  $this->product->findAll();
 
         $data= [
@@ -24,59 +24,74 @@ class ProductController extends BaseController
         return view('product',  $data);
     }
 
-    // public function insertProduct(){
-        
-    //     $data = [
-    //         'nama_product' => 'Smartphone',
-    //         'description' => 'merupakan smartphone merk Samsung'
-    //     ];
+    public function readProductsApi(){
+        $products = $this->product->findAll();
 
-    //     $this->product->insertProduct($data);
-    // }
+        return $this->respond(
+            [
+                'code' => 200,
+                'status' => "OK",
+                'data' => $products
+            ]
+        );
+    }
+    
+    public function getProductApi($id){
+        $product = $this->product->where('id', $id)->first();
+    if (!$product) {
+        $this->response->setStatusCode(404);
+        return $this->response->setJSON(
+            [
+                'code' => 200,
+                'status' => "NOT FOUND",
+                'data' => 'product not found'
+            ]
+            );
+    }
+
+    return $this->respond([
+        'code' => 200,
+        'status' => "OK",
+        'data' => $product
+    ]);
+}
 
     public function insertProductORM()
     {
-    $data = [
-        'nama_product' => $this->request->getPost('nama_product'),
-        'description' => $this->request->getPost('description')
-    ];
+        $data = [
+            'nama_product' => $this->request->getPost('nama_product'),
+            'description' => $this->request->getPost('description')
 
-    $this->product->insertProductORM($data);
+        ];
 
-    return redirect()->to('readproduct');
+        $this->product->insertProductORM($data);
+        return redirect()->to('readproduct');
     }
-
-
     public function insertPage(){
         return view('insertproduct');
     }
-
-    public function getProduct($id)
-    {
+    public function getProduct($id){
         $product = $this->product->find($id);
         $data = [
             'product' => $product
         ];
-    
-        return view("edit-product", $data);
+        return view('edit-product', $data);
     }
-    
 
-    public function updateProduct($id)
-    {
+    public function updateProduct($id){
         $nama_product = $this->request->getVar('nama_product');
         $description = $this->request->getVar('description');
         $data = [
-            'nama_product'=>$nama_product,
-            'description'=>$description
+            'nama_product' => $nama_product,
+            'description' =>  $description
         ];
-        $this->product->update($id,$data);
+        $this->product->update($id, $data);
         return redirect()->to(base_url("readproduct"));
-
     }
-
     public function deleteProduct($id){
         $this->product->delete($id);
         return redirect()->to(base_url("readproduct"));
     }
 }
+
+?>
